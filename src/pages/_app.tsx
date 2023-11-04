cba620ea17446f4ecc0a4200c63febac2780f3fe
danakim530 <danakim530@gmail.com>
2023-11-04 14:19:04 +0900

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import Header from '@/common/Header';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-
+//test
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>

00e29b0a900f6e30ff92402dd741e84605a698de
danakim530 <danakim530@gmail.com>
2023-11-25 14:08:57 +0900

    startpage로 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import PopularTest from '@/components/PopularTest';
 import styles from './Home.module.scss';
+import StartTest from './mbti/soccer/StartTest';
 
 const Home = () => {
   return (
     <div className={styles.MainPage}>
-      <PopularTest />
+      {/* <PopularTest /> */}
+      <StartTest />
     </div>
   );
 };

fccd74d79da330e506681673669d8feb11a1b115
danakim530 <danakim530@gmail.com>
2023-11-25 13:21:55 +0900

    startPage style 수정

--- src/pages/mbti/soccer/StartTest.tsx
+++ src/pages/mbti/soccer/StartTest.tsx
@@ -1,9 +1,9 @@
 import React, { useRef, useState } from 'react';
 import styles from './startTest.module.scss';
-import Button from '@/components/styled-components/Button';
+// import Button from '@/components/styled-components/Button';
 import PopularTest from '@/components/PopularTest';
 import Image from 'next/image';
-import sampleImg from 'public/images/mbtiTest-start.png';
+import main from 'public/images/main_pic.png';
 import router, { useRouter } from 'next/router';
 import { useSetRecoilState } from 'recoil';
 import { userName } from '@/states/userState';
@@ -19,7 +19,7 @@ const StartTest = () => {
       console.log(userNameRef.current.value);
       router.push('/mbti/soccer-test');
     } else {
-      alert('사용자의 아이디 입력 받기 팝업도 생성하자');
+      alert('닉네임을 입력해주세요.');
     }
   };
   return (
@@ -32,20 +32,21 @@ const StartTest = () => {
       }}
     >
       <div className={styles.startTest}>
-        <div className={styles.startTest__title}>여행 스타일로 보는 나의 성격은?</div>
+        <div className={styles.startTest__title}>축구 선수로 알아보는 나의 성격은?</div>
         <div className={styles.startTest__subtitle}>
-          총 16개의 유형의 MBTI 성향을 기반으로 여행 스타일 속 나의 모습을 알아보아요
+          <div>총 16개의 유형의 MBTI 성향을 기반으로</div>
+          <div>나의 성격에 따른 축구 포지션을 알아보아요!</div>
         </div>
-        <Image src={sampleImg} width={'200px'} height={600} alt="샘플 이미지" />
+        <Image src={main} width={'200px'} height={'400px'} alt="샘플 이미지" />
         <input
           className={styles.startTest__input}
           ref={userNameRef}
-          placeholder="이름을 입력해주세요"
+          placeholder="닉네임을 입력해주세요"
           required
         />
-        <form className={styles.startTest__button} onClick={onClickStartTest}>
-          <Button text="테스트하기" />
-        </form>
+        <button className={styles.startTest__button} onClick={onClickStartTest}>
+          테스트하기
+        </button>
       </div>
       <div style={{ maxWidth: '500px' }}>
         <PopularTest />

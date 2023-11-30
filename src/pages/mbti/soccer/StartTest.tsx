3cb09546bfd9d8e08fea28c89678ba6b42dbd07e
ShimSungBo <shimdb96@gmail.com>
2023-11-30 23:15:09 +0900

    feat: errorPopup

--- src/pages/mbti/soccer/StartTest.tsx
+++ src/pages/mbti/soccer/StartTest.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { ChangeEvent, useRef, useState } from 'react';
 import styles from './startTest.module.scss';
 // import Button from '@/components/styled-components/Button';
 import PopularTest from '@/components/PopularTest';
@@ -13,45 +13,61 @@ const StartTest = () => {
   const setUserName = useSetRecoilState(userName);
   const userNameRef = useRef<HTMLInputElement>(null);
 
+  const [modalToggleOn, setModalToggleOn] = useState(false);
+
   const onClickStartTest = () => {
     if (userNameRef?.current?.value) {
       setUserName(userNameRef.current.value);
       console.log(userNameRef.current.value);
       router.push('/mbti/soccer-test');
     } else {
-      alert('닉네임을 입력해주세요.');
+      setModalToggleOn(true);
     }
   };
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <div className={styles.startTest}>
-        <div className={styles.startTest__title}>축구 선수로 알아보는 나의 성격은?</div>
-        <div className={styles.startTest__subtitle}>
-          <div>총 16개의 유형의 MBTI 성향을 기반으로</div>
-          <div>나의 성격에 따른 축구 포지션을 알아보아요!</div>
+    <>
+      {modalToggleOn && (
+        <div className={styles.modalWrap}>
+          <div className={styles.modalWrapBody}>
+            <span className={styles.closeBtn} onClick={() => setModalToggleOn(false)}>
+              &times;
+            </span>
+            <p className={styles.modalText}>닉네임을 입력해주세요!</p>
+          </div>
         </div>
-        <Image src={main} width={'200px'} height={'400px'} alt="샘플 이미지" />
-        <input
-          className={styles.startTest__input}
-          ref={userNameRef}
-          placeholder="닉네임을 입력해주세요"
-          required
-        />
-        <button className={styles.startTest__button} onClick={onClickStartTest}>
-          테스트하기
-        </button>
-      </div>
-      {/* <div style={{ maxWidth: '500px' }}>
+      )}
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <div className={styles.startTest}>
+          <div className={styles.startTest__title}>축구 선수로 알아보는 나의 성격은?</div>
+          <div className={styles.startTest__subtitle}>
+            <div>총 16개의 유형의 MBTI 성향을 기반으로</div>
+            <div>나의 성격에 따른 축구 포지션을 알아보아요!</div>
+          </div>
+          <Image src={main} width={'200px'} height={'400px'} alt="샘플 이미지" />
+          <input
+            className={styles.startTest__input}
+            ref={userNameRef}
+            placeholder="닉네임을 입력해주세요"
+            required
+          />
+          <button className={styles.startTest__button} onClick={onClickStartTest}>
+            테스트하기
+          </button>
+        </div>
+        {/* <div style={{ maxWidth: '500px' }}>
         <PopularTest />
       </div> */}
-    </div>
+      </div>
+    </>
   );
 };
 
